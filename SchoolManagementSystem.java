f89b3836728465869cbb2aa2401ebe03142d078c
abdulsamadexe <abdulsamadbrohi461@gmail.com>
2024-11-22 03:14:33 +0500

    update main message in SchoolManagementSystem

--- SchoolManagementSystem.java
+++ SchoolManagementSystem.java
@@ -1,5 +1,5 @@
 class SchoolManagementSystem {
     public static void main(String[] args) {
-        System.out.println("my branch");
+        System.out.println("School Management System");
     }
 }
